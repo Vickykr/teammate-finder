9f1b462512c842b76b35fdbb6923b16a9740d7b6
Vicky Yadav <vy8896282@gmail.com>
2021-10-30 12:43:48 +0530

    not impt

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(db_url, {
 
 // Session store creates a collection in the DB
 let mongoStore = MongoDbStore.create({
-    mongoUrl: db_url,
+    mongoUrl: db_url
     //mongooseConnection: connection, //name of the Db where we will store session
     //collectionName: 'sessions',  //name of the collection where we will store session
 })
